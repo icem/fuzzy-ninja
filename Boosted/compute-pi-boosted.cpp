5c68401fa0acf7c1d0dc8b80ce4f35fefb3a1a5b
Pavel Perestoronin <eigenein@gmail.com>
2012-10-12 02:06:11 +0300

    boosted got working

--- Boosted/compute-pi-boosted.cpp
+++ Boosted/compute-pi-boosted.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-#include <boost/mpi/environment.hpp>
-#include <boost/mpi/communicator.hpp>
+#include <boost/mpi.hpp>
 
 void print_help()
 {
@@ -16,8 +15,51 @@ int main(int argc, char *argv[])
 
     if (argc != 2)
     {
-        print_help();
+        if (world.rank() == 0)
+        {
+            print_help();
+        }
         return 1;
+    }
+
+    int intervalCount;
+
+    if (world.rank() == 0)
+    {
+        ::std::istringstream(argv[1]) >> intervalCount;
+    }
+
+    ::boost::mpi::broadcast(world, intervalCount, 0);
+
+    ::std::cout << "[" << world.rank() << "] intervalCount: " << intervalCount
+                << ::std::endl;
+
+    int i;
+    double x, h, sum, partialValue;
+
+    h = 1.0 / intervalCount;
+    sum = 0.0;
+    for (i = world.rank() + 1; i <= intervalCount; i += world.size())
+    {
+        x = h * ((double)i - 0.5);
+        sum += 4.0 / (1.0 + x*x);
+    }
+    partialValue = h * sum;
+
+    ::std::cout << "[" << world.rank() << "] partialValue: " << partialValue
+                << ::std::endl;
+
+    if (world.rank() == 0)
+    {
+        double answer;
+
+        reduce(world, partialValue, answer, ::std::plus<double>(), 0);
+        ::std::cout.precision(18);
+        ::std::cout << "Pi: " << answer << ::std::endl;
+    }
+    else
+    {
+        reduce(world, partialValue, ::std::plus<double>(), 0);
     }
 
     return 0;
